61adcc795c05f4adf1a6ec2cdb20d8eaaf920687
Kelly Bowman <KBowman5@CNM.Edu>
2022-09-19 20:08:53 -0600

    Almost done with V1

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Test");
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+
+    form.addEventListener("submit",  (event) => {
+        handleItemForm(event, form);
+    });
+}
+
+function handleItemForm(event, formRef) {
+    if(event.preventDefault()) {
+        event.preventDefault()();
+    }
+
+    let itemHtml = addItemToShoppingList();
+    console.log("Item HTML: ", itemHtml);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+    return false;
+}
+
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+
+
+}
+
+function createListItemHtml() {
+    return '<li>
+                Item Name - Amount
+            </li>
+    ';
+}
